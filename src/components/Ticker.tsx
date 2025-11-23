df7cfcafe917f1e648207fc2555fbe40532c1c0e
wtfPrethiv <prethivshoba@gmail.com>
2025-11-23 13:40:20 +0530

    fix: fixed ticker logic & hid scrollbars

--- src/components/Ticker.tsx
+++ src/components/Ticker.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import './Ticker.css'
+
 const Ticker: React.FC = () => {
-  return (
-    <div className="double00">
+
+  const content = (
+    <div className="ticker-item">
       <p>
         &copy; // DOUBLE00 | wtfPrethiv&nbsp;&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;
-        // WELCOME TO THE PORTFOLIO &nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp; // SCROLL DOWN FOR PROJECTS &nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;
+        // WELCOME TO THE PORTFOLIO &nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp; 
+        // SCROLL DOWN FOR PROJECTS &nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;
         // LAST UPDATED : DEC 7 2024&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;&nbsp;
         // NOTE: ssSSssIiitEESSSSSS NNnnnnOOoooooTTTTtT fuLLY RESPOnSIVE yetttt !!!!!!!!!!!!!!
       </p>
+    </div>
+  );
+
+  return (
+    <div className="double00">
+      <div className="ticker-track">
+        {content}
+        {content}
+      </div>
     </div>
   );
 };
